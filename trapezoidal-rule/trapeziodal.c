2891e511beee71c1becb8e42a658e36d808c1750
Rohman Widiyanto <widiyanto.rohman@gmail.com>
2017-05-02 17:13:00 +0700

    more tidy | add trapezoidal rule

--- trapezoidal-rule/trapeziodal.c
+++ trapezoidal-rule/trapeziodal.c
@@ -4,19 +4,43 @@
 
 const int MAX_STRING = 100;
 
+double f(double x) {
+	double return_val;
+
+	return_val = x * x + 1.0;
+
+	return return_val;
+}
+
+double Trap(double left_endpt, double right_endpt, int trap_count, double base_len) {
+	double estimate, x;
+	int i;
+
+	estimate = (f(left_endpt) + f(right_endpt)) / 2.0;
+
+	for (i = 1; i <= trap_count - 1; i++) {
+		x = left_endpt + i * base_len;
+		estimate += f(x);
+	}
+
+	estimate = estimate * base_len;
+
+	return estimate;
+}
+
 int main(void) {
-  int my_rank, comm_sz, n = 1024, local_n;
-  double a = 0.0, b = 3.0, h, local_a, local_b;
-  double local_int, total_int;
-  int source;
+	int my_rank, comm_sz, n = 1024, local_n;
+	double a = 0.0, b = 3.0, h, local_a, local_b;
+	double local_int, total_int;
+	int source;
 
-  MPI_Init(NULL, NULL);
-  MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
-  MPI_Comm_size(MPI_COMM_WORLD, &comm_sz);
+	MPI_Init(NULL, NULL);
+	MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
+	MPI_Comm_size(MPI_COMM_WORLD, &comm_sz);
 
-  h = (b−a)/n; /* h is the same for all processes */
-  local_n = n/comm_sz; /* So is the number of trapezoids */
-  
+	h = (b - a) / n;
+	local_n = n / comm_sz;
+
   local_a = a + my_rank * local_n * h;
   local_b = local_a + local_n * h;
   local_int = Trap(local_a, local_b, local_n, h);
@@ -31,6 +55,7 @@ int main(void) {
       MPI_Recv(&local_int, 1, MPI_DOUBLE, source, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
       total_int += local_int;
     }
+	}
 
   if (my_rank == 0) {
     printf("With n = %d trapezoids, our estimate\n", n);
